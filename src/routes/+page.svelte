b0d78e140e29d5a4831a287d73c9cc069ee28e06
hyddeos <ericsageby@hotmail.com>
2022-11-28 18:26:06 +0100

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../styles/tailwind-output.css";
+  </script>
+<div>
+    <h1 class="lowercase text-indigo-500">Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the docdwadaumentation</p>
+</div>
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  // ...
+  mode: 'jit', // ⚠ Make sure to have this
+  content: ["./src/**/*.svelte"],
+  theme: {
+    colors: {          
+          
+      "primary": "#f260d2",
+          
+      "secondary": "#f9e12c",
+               
+      "accent": "#93ed49",
+               
+      "neutral": "#141624",
+               
+      "base-100": "#f3f4f6",
+
+      "base-200": "#d1d5db",
+
+      "base-300": "#4b5563",
+
+      "base-400": "#1f2937",
+               
+      "info": "#88AFF7",
+               
+      "success": "#5CE0B8",
+               
+      "warning": "#E48007",
+               
+      "error": "#F14B61",
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+}
