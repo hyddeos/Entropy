6ef8607e47ae2e436ba4ff66c4a19989caf9a1e2
hyddeos <ericsageby@hotmail.com>
2022-12-05 18:09:34 +0100

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,7 @@
+import adapter from '@sveltejs/adapter-node';
 
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-
-const config = {
-	kit: {
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter()
+  }
 };
-
-export default config;
