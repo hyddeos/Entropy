9230a0703c8b498f05d084d884828b87e7d66fda
hyddeos <ericsageby@hotmail.com>
2022-12-05 17:33:34 +0100

    static test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
     import "../styles/tailwind-output.css";    
 </script>
   <!-- Try some classes here -->
+  
   <body class="bg-neutral min-h-full p-5 text-base-200">
+    
       <slot></slot>    
   </body>
