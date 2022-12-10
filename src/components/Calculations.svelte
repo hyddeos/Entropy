c976be38c43bde9e327aa4b798eaaf6ea47ca72f
hyddeos <ericsageby@hotmail.com>
2022-12-10 17:42:38 +0100

    math works

--- src/components/Calculations.svelte
+++ src/components/Calculations.svelte
@@ -1,19 +1,34 @@
 <script>
-	import { element } from "svelte/internal";
+	import { current_component, element } from "svelte/internal";
     export let active_particles;
     export let boxLeft;
     export let boxRight;
 
+    let currentEntropy = 0;
+
+    // Factorial
+    function f(n) {
+        return (n != 1) ? n * f(n - 1) : 1;
+    }
+
     $: {
-        console.log("Left",  boxLeft.length)
-        console.log("Right",  boxRight.length, typeof(boxRight))
-    } 
+        if (boxLeft.length > 0) {
+            if (active_particles - boxLeft.length > 0) {
+                let tot = f(active_particles) / (f(boxLeft.length) * f(active_particles - boxLeft.length) ) ;
+                currentEntropy = tot
+            }
+            else {
+                currentEntropy = 1
+            }
+            }
+    }
    
 	
 </script>
 
 <div class="text-center">
-    Calc Tot: {active_particles}, Left: {boxLeft.length}, Right {boxRight.length}
-    Current: X nr of diffent combinations is possible for this state
-    Maximum: 1 possible combiantion is possible for this state
+    Calc Tot: {active_particles}
+
+   <p>Current: { currentEntropy } nr of diffent combinations is possible for this state</p> 
+
 </div>
