fc748f518ccce2ef2c7fee9b89e4b2cdc85e3e7b
eric <eric@grafoma.se>
2025-09-22 08:50:54 +0000

    updated some text and checked the logic

--- src/components/Calculations.svelte
+++ src/components/Calculations.svelte
@@ -1,31 +1,32 @@
 <script>
-	import { current_component, element } from "svelte/internal";
     export let active_particles;
     export let boxLeft;
 
     let currentEntropy = 0;
 
-    // Factorial
+    // Recursive factorial function that correctly handles n=0
     function f(n) {
-        return (n != 1) ? n * f(n - 1) : 1;
+        if (n < 0) return 0; // Guard against negative numbers
+        return (n > 1) ? n * f(n - 1) : 1;
     }
 
     $: {
-        if (boxLeft.length > 0) {
-            if (active_particles - boxLeft.length > 0) {
-                let tot = f(active_particles) / (f(boxLeft.length) * f(active_particles - boxLeft.length) ) ;
-                currentEntropy = tot
-            }
-            else {
-                currentEntropy = 1
-            }
-            }
+        let n = active_particles;
+        let k = boxLeft.length;
+        
+        // The number of microstates (Ω) for a given macrostate (k particles in one box, n-k in the other)
+        // is given by the binomial coefficient "n choose k".
+        // Ω = n! / (k! * (n-k)!)
+        if (n >= k && k >= 0) {
+             currentEntropy = f(n) / (f(k) * f(n - k));
+        } else {
+             // This case should ideally not be reached in normal operation.
+             currentEntropy = 0;
+        }
     }
-   
-	
 </script>
 
 <div class="text-center">
-   <h4 class="my-1 text-xl"><strong class="bold underline decoration-success">{ currentEntropy }</strong> possible {#if currentEntropy === 1} configuration{:else} configurations{/if}*</h4>
-   <p class="my-1" > </p>
+   <h4 class="my-1 text-xl"><strong class="bold underline decoration-success">{ currentEntropy }</strong> possible {#if currentEntropy === 1} configuration{:else} system configurations{/if} like this.</h4>
+   <p class="my-1" >The number of other possible configurations that has the same number of particles on each side. </p>
 </div>
